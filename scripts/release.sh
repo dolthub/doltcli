4bc78593d163214cab8c98f4540b5e9bb861e6d9
Max Hoffman <max@dolthub.com>
2021-03-10 13:15:31 -0800

    chmod +x again

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,10 +6,10 @@ DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 BASE=$DIR/..
 
 USERNAME=Dolthub
-#PASSWORD=
+PASSWORD=
 
 poetry config http-basic.pypi $USERNAME $PASSWORD
 
-cd $BASE
-poetry build
-poetry publish -r doltcli $@
+#cd $BASE
+#poetry build
+#poetry publish -r doltcli $@
