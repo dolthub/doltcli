04c18adddc4f6728693b908578aa0b36f4c454ad
Sam Richards <sammyrichards@me.com>
2024-01-05 13:44:30 -0500

    Allow ~ and fix for schema_export (#46)
    * Update dolt.py
    
    * Update schema_export to match cli
    
    the --filename flag appears to have been removed from the cli
    
    * Add test
    
    * Fix the test

--- doltcli/dolt.py
+++ doltcli/dolt.py
@@ -267,6 +267,8 @@ class Dolt(DoltT):
     """
 
     def __init__(self, repo_dir: str, print_output: Optional[bool] = None):
+        # allow ~ to be used in paths
+        repo_dir = os.path.expanduser(repo_dir)
         self.repo_dir = repo_dir
         self._print_output = print_output or False
 
@@ -1366,7 +1368,7 @@ class Dolt(DoltT):
         args = ["schema", "export", table]
 
         if filename:
-            args.extend(["--filename", filename])
+            args.extend([filename])
             _execute(args, self.repo_dir)
             return True
         else:
